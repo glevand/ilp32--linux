082a6a1d8e95c1de56242c1a2a084342de2cb8c8
Geoff Levand <geoff@infradead.org>
2020-03-03 16:44:19 -0800

    vsdo: sizeof_types
    Signed-off-by: Geoff Levand <geoff@infradead.org>

--- arch/arm64/kernel/vdso_ilp32/vgettimeofday.c
+++ arch/arm64/kernel/vdso_ilp32/vgettimeofday.c
@@ -8,6 +8,28 @@
 #include <linux/time.h>
 #include <linux/types.h>
 
+#if 0
+static void sizeof_types(void)
+{
+	struct __kernel_timespec ts;
+	struct old_timespec32 ots32;
+	struct __kernel_old_timeval otv;
+	struct __kernel_old_timeval otv32;
+
+	char (*__1)[sizeof(ts.tv_sec)] = 1;     // 8 bytes.
+	char (*__2)[sizeof(ts.tv_nsec)] = 1;    // 8 bytes.
+
+	char (*__3)[sizeof(ots32.tv_sec)] = 1;  // 4 bytes.
+	char (*__4)[sizeof(ots32.tv_nsec)] = 1; // 4 bytes.
+
+	char (*__5)[sizeof(otv.tv_sec)] = 1;    // 4 bytes.
+	char (*__6)[sizeof(otv.tv_usec)] = 1;   // 4 bytes.
+
+	char (*__7)[sizeof(otv32.tv_sec)] = 1;  // 4 bytes.
+	char (*__8)[sizeof(otv32.tv_usec)] = 1; // 4 bytes.
+}
+#endif
+
 int __kernel_clock_gettime(clockid_t clock,
 			   struct __kernel_timespec *ts)
 {
